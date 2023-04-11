bd6e57a9bd12da66b2084cc2fefe5ad699978a4e
mohmj <mohizuu.99@gmail.com>
2023-04-11 23:35:13 +0300

    Update project

--- Express/RedirectApp/urls.py
+++ Express/RedirectApp/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[path('',views.RedirectFunction)]
+urlpatterns=[
+    path('',views.RedirectFunction),
+    path('login', views.LoginPage),
+    path('loginPost', views.LoginPost)
+]
--- Express/RedirectApp/views.py
+++ Express/RedirectApp/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, redirect
 import sys
 from django.http import HttpRequest
-from pyrebase import pyrebase
-import firebase_admin
-from firebase_admin import firestore
+# from pyrebase import pyrebase
 firebaseConfig = {
   "apiKey": "AIzaSyA0G9sz2UgL2JonD2bdRSPoaW1hLvvhrHE",
   "authDomain": "rq-express-dev.firebaseapp.com",
@@ -13,9 +11,22 @@ firebaseConfig = {
   "appId": "1:841781456816:web:b702055f9fd74f83c8b075",
   "measurementId": "G-YKEL44CZEH"
 }
-# sys.setrecursionlimit(10000)
+
+# firebase=pyrebase.initialize_app(firebaseConfig)
+# auth=firebase.auth()
 def RedirectFunction(request):
     # famous = request.GET.get("famous") or "no famous assigned"
     website = request.GET.get("website") or "https://google.com"
     return redirect(website)
     # return render(request,"RedirectApp_index.html",{"famous":famous,"website":website})
+
+def LoginPage(request):
+    return render(request, "login_page.html")
+
+def LoginPost(request):
+    email=request.POST.get('email')
+    password=request.POST.get('password')
+    # auth.sign_in_with_email_and_password(email,password)
+    # if(auth.current_user != None):
+    #     return render(request,"home.html",{"message":"user logged in successfully",})
+    return render(request,"home.html",{"message":"user failed login",})
