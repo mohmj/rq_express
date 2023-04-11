ad451ff39ce22e0339608a1c69abbd2cf31c4f8d
mohmj <mohizuu.99@gmail.com>
2023-04-11 17:01:09 +0300

    Initial commit

---
+++ Express/RedirectApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+# from pyrebase import pyrebase
+
+
+def redirect(request):
+    famous = request.GET.get("famous") or "no famous assigned"
+    website = request.GET.get("website") or "no website addigned"
+    return render("test working correctly, {0} & {1}".format(famous,website))
