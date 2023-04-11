4342af02155caee8654acefad5a858acf672ce12
mohmj <mohizuu.99@gmail.com>
2023-04-11 23:49:24 +0300

    Update views.py

--- Express/RedirectApp/views.py
+++ Express/RedirectApp/views.py
@@ -1,19 +1,6 @@
 from django.shortcuts import render, redirect
-import sys
 from django.http import HttpRequest
-# from pyrebase import pyrebase
-firebaseConfig = {
-  "apiKey": "AIzaSyA0G9sz2UgL2JonD2bdRSPoaW1hLvvhrHE",
-  "authDomain": "rq-express-dev.firebaseapp.com",
-  "projectId": "rq-express-dev",
-  "storageBucket": "rq-express-dev.appspot.com",
-  "messagingSenderId": "841781456816",
-  "appId": "1:841781456816:web:b702055f9fd74f83c8b075",
-  "measurementId": "G-YKEL44CZEH"
-}
 
-# firebase=pyrebase.initialize_app(firebaseConfig)
-# auth=firebase.auth()
 def RedirectFunction(request):
     # famous = request.GET.get("famous") or "no famous assigned"
     website = request.GET.get("website") or "https://google.com"
