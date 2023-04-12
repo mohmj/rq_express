62f713d2169ff81d94534af8bdff0e7a83521c20
mohmj <mohizuu.99@gmail.com>
2023-04-12 07:11:58 +0300

    Update views in campaingsapp

--- Express/CampaignsApp/views.py
+++ Express/CampaignsApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def newCampaign(request):
-    return render(request,"CampaignsApp/new_campaign.html")
+    return render(request,"CampaignsApp/templates/new_campaign.html")
