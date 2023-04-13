35b7fa274dabce26af9d9f9b1ab46b0be9e72f64
mohmj <mohizuu.99@gmail.com>
2023-04-13 18:53:49 +0300

    Update

--- Express/ControlApp/urls.py
+++ Express/ControlApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.indexPage),
     path('campaigns', views.campaignsPage),
-    path('famous', views.famousPage)
+    path('famous', views.famousPage),
+    path('campaigns/campaignShow',views.showCampaign),
 ]
--- Express/ControlApp/views.py
+++ Express/ControlApp/views.py
@@ -1,13 +1,27 @@
+import datetime
+
 from django.shortcuts import render,redirect
 import firebase_config
 
 def indexPage(request):
     return render(request,"ControlApp/index.html")
 
+campaignsArray=[]
 def campaignsPage(request):
-     # docs=firebase_config.firestore_client.collection("campaigns").stream
-    return render(request,"ControlApp/campaigns.html")
+     campaignsArray.clear()
+     docs = firebase_config.firestore_client.collection('campaigns').stream()
+     for doc in docs:
+         campaignsDict = doc.to_dict()
+         campaignsArray.append({"name_ar": campaignsDict['name_ar'], "name_en": campaignsDict['name_en'], "link":campaignsDict['link'], "start_time":str(campaignsDict['start_time'].toDate()), "end_time":"0"})
+     return render(request,"ControlApp/campaigns.html",{"campaignsArray":campaignsArray})
+
+def showCampaign(request):
+
+    # id=request.POST.get("id")
+    # print("The id: "+id)
+    campaignData=firebase_config.firestore_client.collection("campaigns").document("sinjar_eid").get()
+    print("The type is"+str(type(campaignData)))
+    return (request,"ControlApp/campaign_show",{"campaign":campaignData})
 
 def famousPage(request):
     return render(request,"ControlApp/famous.html")
-
