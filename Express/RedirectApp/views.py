bafec61a700ef87cdad570998112f4fd77c8cf2f
mohmj <mohizuu.99@gmail.com>
2023-04-16 08:11:14 +0300

    U

--- Express/RedirectApp/views.py
+++ Express/RedirectApp/views.py
@@ -20,12 +20,12 @@ def RedirectFunction(request):
     firebase_config.firestore_client.collection("campaigns").document(campaign).set({
         "famous."+famous:{
             "views":firebase_config.Increment(1),
-            "device."+str(user_agent.device.brand):firebase_config.Increment(1),
-            "Browser.":{str(user_agent.browser.family):firebase_config.Increment(1)},
-            "OS.":{str(user_agent.os.family):firebase_config.Increment(1)},
+            "device":{str(user_agent.device.brand):firebase_config.Increment(1)},
+            "Browser":{str(user_agent.browser.family):firebase_config.Increment(1)},
+            "OS":{str(user_agent.os.family):firebase_config.Increment(1)},
         }
 
-    })
+    }, merge=True)
     firebase_config.firestore_client.collection("famous").document(famous).update({
         "campaigns."+campaign: firebase_config.Increment(1)
     })
