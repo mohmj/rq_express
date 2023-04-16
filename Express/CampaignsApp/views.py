599925f8d29fc81b70ec5aebe3eaad8e6dee319e
mohmj <mohizuu.99@gmail.com>
2023-04-16 03:29:09 +0300

    Update CampaignApp/views for testing on server

--- Express/CampaignsApp/views.py
+++ Express/CampaignsApp/views.py
@@ -14,7 +14,7 @@ def newCampaign(request):
 
 def createNewCampaign(request):
     user_agent=get_user_agent(request)
-    return HttpResponse("<h3>"+str(user_agent.device)+"</h3>\n<h3><"+str(user_agent.browser)+"</h3>\n<h3><"+str(user_agent.os)+"</h3>")
+    return HttpResponse("<h3>"+str(user_agent.device)+"</h3>\n<h3>"+str(user_agent.browser)+"</h3>\n<h3>"+str(user_agent.os)+"</h3>")
     name_arabic=request.POST.get('name_Arabic')
     name_english=request.POST.get('name_English')
     link=request.POST.get('link')
