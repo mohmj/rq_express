893512399df71cf1eb646ea1e3b0fc4de8fe0bbd
mohmj <mohizuu.99@gmail.com>
2023-04-24 15:55:19 +0300

    Update application

--- Express/ControlApp/models.py
+++ Express/ControlApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name=models.CharField(max_length=256)
+    language=models.CharField(max_length=3)
+    price=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
--- Express/Express/settings.py
+++ Express/Express/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_agents'
+    'user_agents',
+    'rest_framework',
+    'ControlApp',
 ]
 
 MIDDLEWARE = [
